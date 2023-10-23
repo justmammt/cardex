aa42cb621ac4c953f424d3e47a37cbf069a02858
justmammt <justmammt69@gmail.com>
2023-10-23 22:00:06 +0200

    Fixed 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ContactView from '../views/ContactView.vue'
+import NotFoundView from '../components/NotFoundPage.vue'
 
 const routes = [
   {
@@ -10,15 +12,12 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }, {
-    path: '/*',
-    name: '404',
-    component: () => import('../views/NotFound.vue')
-
+    component: ContactView
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'not-found',
+    component: NotFoundView
   }
 ]
 
@@ -26,5 +25,4 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
-
 export default router
