ecdfa3e1d984176fb182621b5c10b782687f545c
justmammt <justmammt69@gmail.com>
2023-10-24 22:27:00 +0200

    modificato:             .gitignore 	modificato:             package.json 	modificato:             src/components/NotFoundPage.vue 	modificato:             src/views/ContactView.vue 	modificato:             yarn.lock

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,13 @@
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="py-12">
+    <div class="items-center max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="text-center">
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact Us</h2>
+        <p class="mt-3 max-w-2xl mx-auto text-lg text-gray-500 sm:mt-4">Have a doubt or want to give a feedback? We'd love to hear from you!</p>
+      </div>
+    </div>
   </div>
 </template>
+
+
