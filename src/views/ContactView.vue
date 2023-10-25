dc1c2e3f2e31a61fcd0e870117cd3550b3449f47
justmammt <justmammt69@gmail.com>
2023-10-25 18:21:28 +0200

    Fontawesome & Contact
    Added Fontawesome directory and initialized contact page, footer also added (no function)

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,13 +1,56 @@
-
 <template>
   <div class="py-12">
     <div class="items-center max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="text-center">
-        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact Us</h2>
-        <p class="mt-3 max-w-2xl mx-auto text-lg text-gray-500 sm:mt-4">Have a doubt or want to give a feedback? We'd love to hear from you!</p>
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Contact Us
+        </h2>
+        <p class="mt-3 max-w-2xl mx-auto text-lg text-gray-500 sm:mt-4">
+          Have a doubt or want to give a feedback? We'd love to hear from you!
+        </p>
+      </div>
+    </div>
+    <div class="items-center max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="p-12 relative grid grid-cols-1 md:grid-cols-3 gap-3">
+        <div
+          class="shadow-hover-animation px-2 hover:shadow-2xl shadow-inner rounded-lg"
+        >
+          <div class="text-center">
+            <div>
+              <i
+                class="fa-regular fa-envelope sm:py-3 py-2 header-icon fa-bounce"
+              ></i>
+
+              <h1
+                class="font-bold text-center header"
+                style="padding-bottom: 10px"
+              >
+                Email
+              </h1>
+              <span class="text-left"
+                >If you'd like to leave a feedback, or have any questions, feel
+                free to contact the email below</span
+              >
+              <div class="py-3">
+                <button
+                  class="py-1 px-2 bg-blue-500 hover:bg-blue-600 rounded-xl"
+                >
+                  <a class="button-text-size text-white font-bold"
+                    >justnorganization@justmammtlol.wtf</a
+                  >
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>02</div>
+        <div>03</div>
       </div>
     </div>
   </div>
 </template>
 
-
+<style lang="scss" scoped>
+@import "@/assets/fontawesome/css/all.css";
+@import "@/styles/ContactView.scss";
+</style>
