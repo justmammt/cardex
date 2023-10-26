1d633071e06ec7a7d451500217623c3278b4d9a4
justmammt <justmammt69@gmail.com>
2023-10-26 19:03:34 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,6 @@ module.exports = {
     parser: "@babel/eslint-parser",
   },
   rules: {
-    "no-control-regex": 0,
-    "no-unused-vars": true,
-    "no-undef": true,
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
