bdf9352dd8c282729c2b595e12028f45543bbe4b
justmammt <justmammt69@gmail.com>
2023-10-26 14:53:15 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ContactView from "../views/ContactView.vue";
 import NotFoundView from "../components/NotFoundPage.vue";
@@ -29,7 +29,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory("/"),
+  history: createWebHistory(),
   routes,
 });
 
